ab6b2087a14439935a7d1512ac145a19441b5cc1
Emilly B. Vetorazzi <studyemiemi@gmail.com>
2025-08-19 21:00:31 -0300

    Adicionado o WHILE para fazer a pergunta e dar a dica (maior/menor) até acertar

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 alert("Bem vindo ao jogo do número secreto!");
-let numeroSecreto = Math.random(1, 30);
-let chute = prompt("Escolha um número entre 1 e 50");
+let numeroSecreto = 10;
+let chute
 
-// Valida se o número está entre a margem permitida
+/* Valida se o número está entre a margem permitida
 if (chute > 50 || chute < 50) {
     alert("Seu chute está fora da margem")
-} 
+} */
 
-// Valida se o jogador acertou
-if (numeroSecreto == chute) {
-    alert("Boa! Você acertou!");
-} else if (numeroSecreto > chute) {
-    alert("O número secreto é maior. Tente novamente!");
-} else if (numeroSecreto < chute) {
-    alert("O número secreto é menor. Tente novamente!");
+//while = enquanto (loop). Enquanto o número for diferente do chute, esse código irá ser executado
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e 10`);
+
+    if (numeroSecreto == chute) {
+        alert(`Boa! Você acertou!`);
+    } else {
+        if (numeroSecreto > chute) {
+        alert(`O número secreto é maior. Tente novamente!`);
+        } else {
+            alert(`O número secreto é menor. Tente novamente!`);
+        }
+    }
 }
