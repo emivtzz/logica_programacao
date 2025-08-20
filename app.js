ddcbacd3ad5d00707c9bcef608ddf097620f792c
Emilly B. Vetorazzi <studyemiemi@gmail.com>
2025-08-19 21:53:57 -0300

    Adicionado o número de tentativas que o jogador precisou para acertar (o acerto conta como tentativa)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 alert("Bem vindo ao jogo do número secreto!");
 let numeroSecreto = 10;
-let chute
+let chute;
+let tentativas = 1;
 
 /* Valida se o número está entre a margem permitida
 if (chute > 50 || chute < 50) {
@@ -12,12 +13,13 @@ while (chute != numeroSecreto) {
     chute = prompt(`Escolha um número entre 1 e 10`);
 
     if (numeroSecreto == chute) {
-        alert(`Boa! Você acertou!`);
+        alert(`Boa! Você acertou o número secreto: ${numeroSecreto} com ${tentativas} tentativas`);
     } else {
         if (numeroSecreto > chute) {
         alert(`O número secreto é maior. Tente novamente!`);
         } else {
             alert(`O número secreto é menor. Tente novamente!`);
         }
+        tentativas++ //tentativas = tentativas + 1
     }
 }
